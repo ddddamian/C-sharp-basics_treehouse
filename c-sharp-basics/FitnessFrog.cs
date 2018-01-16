2ae25c5d947ba76b7100b5a1998d9607d984400d
Pete <peterwalsh86@hotmail.com>
2018-01-16 13:29:59 +0000

    Update FitnessFrog.cs
    Added exception handling, using try & catch.

--- c-sharp-basics/FitnessFrog.cs
+++ c-sharp-basics/FitnessFrog.cs
@@ -7,7 +7,7 @@ namespace Treehouse.FitnessFrog
     static void Main()
     {
       // initialise
-      int runningTotal = 0;
+      double runningTotal = 0;
       bool keepGoing = true;
       
       while(keepGoing) {
@@ -16,45 +16,60 @@ namespace Treehouse.FitnessFrog
         string entry = Console.ReadLine();
         
         // check if the user wants to quit
-        if(entry == "quit")
+        if(entry.ToLower() == "quit")
         {
           keepGoing = false;
         }
         else
         {
           // Add minutes exercised to total
-          int minutes = int.Parse(entry);
           
-          // Check how many minutes the user has entered and give positive feedback
-          if(minutes <= 0)
+          // Exception handling to check that an int or double is entered.
+          try
           {
-            Console.WriteLine(minutes + " is not an acceptable value.");
+            double minutes = double.Parse(entry);
+            
+            // Check how many minutes the user has entered and give positive feedback
+            
+            // if a negative number is used, try again
+            if(minutes <= 0)
+            {
+              Console.WriteLine(minutes + " is not an acceptable value.");
+              continue;
+            }
+            else if(minutes <= 10)
+            {
+              Console.WriteLine("Better than nothing, am I right?");
+            }
+            else if(minutes <= 30)
+            {
+              Console.WriteLine("Way to go hot stuff!");
+            }
+            else if(minutes <= 60)
+            {
+              Console.WriteLine("You must be a ninja warrior in training!");
+            }
+            else
+            {
+              Console.WriteLine("Okay, now you're just showing off!");
+            }
+            
+            // add the minutes entered to the running total
+            runningTotal += minutes;
+            
+            //Display total minutes exercised to the screen
+            Console.WriteLine("You've exercised " + runningTotal + " minutes");
+            
+            // Repeat until the user quits
+          }
+          // handle the exception is non-numeric text is enetered
+          catch(FormatException)
+          {
+            Console.WriteLine("That is not valid input.");
             continue;
           }
-          else if(minutes <= 10)
-          {
-            Console.WriteLine("Better than nothing, am I right?");
-          }
-          else if(minutes <= 30)
-          {
-            Console.WriteLine("Way to go hot stuff!");
-          }
-          else if(minutes <= 60)
-          {
-            Console.WriteLine("You must be a ninja warrior in training!");
-          }
-          else
-          {
-            Console.WriteLine("Okay, now you're just showing off!");
-          }
           
-          // add the minutes entered to the running total
-          runningTotal += minutes;
           
-          //Display total minutes exercised to the screen
-          Console.WriteLine("You've exercised " + runningTotal + " minutes");
-          
-          // Repeat until the user quits
         }
       }
       Console.WriteLine("Goodbye");
