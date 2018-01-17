b94a409e3dd7e60ac4f2df3a8ed60cde83079150
Pete <peterwalsh86@hotmail.com>
2018-01-17 21:37:11 +0000

    Update CodeChallenge.cs

--- c-sharp-basics/CodeChallenge.cs
+++ c-sharp-basics/CodeChallenge.cs
@@ -6,10 +6,21 @@ namespace Treehouse.CodeChallenges
     {
         static void Main()
         {
+            // initialise
+            var number = 0;
+            
             Console.Write("Enter the number of times to print \"Yay!\": ");
             var entry = Console.ReadLine();
             
-            var number = int.Parse(entry);
+            try
+            {
+                number = int.Parse(entry);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("You must enter a whole number.");
+            }
+            
             
             while(number > 0)
             {
