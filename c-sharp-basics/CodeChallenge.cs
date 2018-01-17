2b6da72f66145ddc84793220c60770e1bb6a103d
Pete <peterwalsh86@hotmail.com>
2018-01-17 21:45:17 +0000

    Update CodeChallenge.cs

--- c-sharp-basics/CodeChallenge.cs
+++ c-sharp-basics/CodeChallenge.cs
@@ -12,16 +12,24 @@ namespace Treehouse.CodeChallenges
             Console.Write("Enter the number of times to print \"Yay!\": ");
             var entry = Console.ReadLine();
             
+            // try/catch exception handling for non-numeric inputs
             try
             {
+                // convert string to an integer
                 number = int.Parse(entry);
+                
+                // input validation to check for a positive number
+                if(number < 0)
+                {
+                    Console.Write("You must enter a positive number.");
+                }
             }
             catch(FormatException)
             {
                 Console.WriteLine("You must enter a whole number.");
             }
             
-            
+            // write "Yay!" as many times as the user has input
             while(number > 0)
             {
                 Console.WriteLine("Yay!");
