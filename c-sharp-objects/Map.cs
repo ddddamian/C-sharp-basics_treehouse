f6be308f7f36ccc9684cea12777f360cb322b181
Pete <peterwalsh86@hotmail.com>
2018-01-20 19:56:39 +0000

    Update Map.cs

--- c-sharp-objects/Map.cs
+++ c-sharp-objects/Map.cs
@@ -2,6 +2,17 @@ namespace TreehouseDefense
 {
   class Map
   {
+    // 'public' variables can be accessed by other classes
+    // but they cannot be changed after they've been created
+    // due to being 'readonly'
+    public readonly int Width;
+    public readonly int Height;
     
+    // constructor method
+    public Map(int width, int height)
+    {
+      Width = width;
+      Height = height;
+    }
   }
 }
