119694ae8f56a07a4c5d1ba2e894924b92a6d66c
Pete <peterwalsh86@hotmail.com>
2018-01-24 23:00:44 +0000

    Update Point.cs

--- c-sharp-objects/Point.cs
+++ c-sharp-objects/Point.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TreehouseDefense
 {
   class Point
@@ -9,6 +11,17 @@ namespace TreehouseDefense
     {
       X = x;
       Y = y;
+    }
+    
+    public int DistanceTo(int x, int y)
+    {
+      int xDiff = X - x;
+      int yDiff = Y - y;
+      
+      int xDiffSquared = xDiff * xDiff;
+      int yDiffSquared = yDiff * yDiff;
+      
+      return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
     }
   }
 }
