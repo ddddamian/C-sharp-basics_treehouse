a6c67c67ad162adcf542d0e97147d39cca45d692
Pete <peterwalsh86@hotmail.com>
2018-01-25 23:23:37 +0000

    Update Game.cs

--- c-sharp-objects/Game.cs
+++ c-sharp-objects/Game.cs
@@ -8,9 +8,20 @@ namespace TreehouseDefense
         {
           Map map = new Map(8, 5);
           
-          Point point = new Point(4, 2);
+          Point x = new MapLocation(4, 2);
           
-          Console.WriteLine(point.DistanceTo(5, 5));
+          Point p = x;
+          
+          map.OnMap(new MapLocation(0, 0));
+          
+          Console.WriteLine(x.DistanceTo(5, 5));
+          
+          Console.WriteLine(x is MapLocation);
+          Console.WriteLine(x is Point);
+          
+          Point point = new Point(0, 0);
+          
+          Console.WriteLine(point is MapLocation);
         }
     }
 }
