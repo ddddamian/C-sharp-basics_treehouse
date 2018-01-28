00d1eeedb803bd5ed75f2a5e55ca29f0e6c72567
Pete <peterwalsh86@hotmail.com>
2018-01-28 20:48:33 +0000

    Update Game.cs

--- c-sharp-objects/Game.cs
+++ c-sharp-objects/Game.cs
@@ -8,20 +8,14 @@ namespace TreehouseDefense
         {
           Map map = new Map(8, 5);
           
-          Point x = new MapLocation(4, 2);
-          
-          Point p = x;
-          
-          map.OnMap(new MapLocation(0, 0));
-          
-          Console.WriteLine(x.DistanceTo(5, 5));
-          
-          Console.WriteLine(x is MapLocation);
-          Console.WriteLine(x is Point);
-          
-          Point point = new Point(0, 0);
-          
-          Console.WriteLine(point is MapLocation);
+          try
+          {
+            MapLocation mapLocation = new MapLocation(20, 20, map);
+          }
+          catch(Exception)
+          {
+            Console.WriteLine("That map location is not on the map!");
+          }
         }
     }
 }
