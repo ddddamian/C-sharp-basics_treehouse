7f692fc00de7247d749f3913e20101416ff4d200
Pete <peterwalsh86@hotmail.com>
2018-01-28 20:49:02 +0000

    Update MapLocation.cs

--- c-sharp-objects/MapLocation.cs
+++ c-sharp-objects/MapLocation.cs
@@ -1,10 +1,15 @@
+//using System;
+
 namespace TreehouseDefense
 {
  class MapLocation : Point
  {
-   public MapLocation(int x, int y) : base(x, y)
+   public MapLocation(int x, int y, Map map) : base(x, y)
    {
-     
+     if(!map.OnMap(this))
+     {
+      throw new System.Exception();
+     }
    }
  }
 }
