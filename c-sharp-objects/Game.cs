5875a0747d5d3a6515598b0e920f3637982fdeaa
Pete <peterwalsh86@hotmail.com>
2018-01-29 18:21:15 +0000

    Update Game.cs

--- c-sharp-objects/Game.cs
+++ c-sharp-objects/Game.cs
@@ -12,9 +12,9 @@ namespace TreehouseDefense
           {
             MapLocation mapLocation = new MapLocation(20, 20, map);
           }
-          catch(Exception)
+          catch(Exception ex)
           {
-            Console.WriteLine("That map location is not on the map!");
+            Console.WriteLine(ex.Message);
           }
         }
     }
