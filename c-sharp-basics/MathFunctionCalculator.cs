73ee35c4d54a3fde0682ae51e60b3dfe211ad9a3
Pete <peterwalsh86@hotmail.com>
2018-02-05 20:25:42 +0000

    Create MathFunctionCalculator.cs

---
+++ c-sharp-basics/MathFunctionCalculator.cs
@@ -0,0 +1,5 @@
+//Math Function Calculator
+
+//Prompt the user for a math function such as sine, cosine, tangent, square root, logarithm, etc. Then prompt the user for the function's parameter. Perform the function and print the result back to the screen.
+
+//Use .NET’s Math class to do perform the math for you. 
