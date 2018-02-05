9eb8844a915f11f67cd6e99202296f6433d27efa
Pete <peterwalsh86@hotmail.com>
2018-02-05 20:22:48 +0000

    Create ArithmeticCalculator.cs

---
+++ c-sharp-basics/ArithmeticCalculator.cs
@@ -0,0 +1,12 @@
+//Arithmetic Calculator
+
+//Create a calculator that does one arithmetic operation at a time and prints the result to the screen.
+
+//    Prompt the user for a number.
+//    Prompt the user for an operation (+ - / *).
+//    Prompt the user for another number.
+//    Perform the operation.
+//    Print the result to the screen.
+//    Repeat until the user types in “quit” at any of the prompts.
+
+//Extra Credit: Add the power operator using the ^ symbol. You can calculate a number raised to a power using the Math.Pow method. 
