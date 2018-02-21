fe2032e681441cfe629042353803301e64efeb1e
Pete <peterwalsh86@hotmail.com>
2018-02-21 20:00:49 +0000

    Update ArithmeticCalculator.cs

--- c-sharp-basics/ArithmeticCalculator.cs
+++ c-sharp-basics/ArithmeticCalculator.cs
@@ -51,7 +51,10 @@ namespace ArithmeticCalculator
         {
           result = number1 / number2;
         }
-        
+        else if (operation == '*')
+        {
+          result = number1 * number2;
+        }
         //    Print the result to the screen.
         Console.WriteLine(result);
       }
