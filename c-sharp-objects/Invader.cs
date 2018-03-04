c8f77afb6866cde8d48a9a8264f139b30a21eda2
Pete <peterwalsh86@hotmail.com>
2018-03-04 12:03:06 +0000

    Update Invader.cs

--- c-sharp-objects/Invader.cs
+++ c-sharp-objects/Invader.cs
@@ -2,6 +2,17 @@ namespace TreehouseDefense
 {
   class Invader
   {
+    private MapLocation _location;
     
+    // method to get location
+    public MapLocation GetLocation()
+    {
+      return _location;
+    }
+    //method to set location
+    public void SetLocation(MapLocation value)
+    {
+      _location = value;
+    }
   }
 }
