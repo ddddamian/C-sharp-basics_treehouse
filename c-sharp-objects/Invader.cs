01b2dbc1f60a78e1cc6bfd468539f4f0e7ef4e27
Pete <peterwalsh86@hotmail.com>
2018-03-13 20:55:11 +0000

    Update Invader.cs

--- c-sharp-objects/Invader.cs
+++ c-sharp-objects/Invader.cs
@@ -5,22 +5,13 @@ namespace TreehouseDefense
     private readonly Path _path;
     private int _pathStep = 0;
     
-    public MapLocation Location 
-    { 
-      get
-      {
-        return _path.GetLocationAt(_pathStep);
-      }
-    }
+    public MapLocation Location => _path.GetLocationAt(_pathStep);
     
     public Invader(Path path)
     {
       _path = path;
     }
     
-    public void Move()
-    {
-      _pathStep += 1;     
-    }
+    public void Move() _pathStep += 1; 
   }
 }
