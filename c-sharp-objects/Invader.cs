fa85ead7cc853d7f0aba8f212dba15490c89eaa8
Pete <peterwalsh86@hotmail.com>
2018-03-15 13:00:47 +0000

    Update Invader.cs

--- c-sharp-objects/Invader.cs
+++ c-sharp-objects/Invader.cs
@@ -9,12 +9,21 @@ namespace TreehouseDefense
     
     public int Health { get; private set; } = 2;
     
+    // true if the invader has reached the end of the path
+    public bool HasScored { get { return _pathStep >= _path.Length; } }
+    
+    // true if the invader's health is 0 or less.
+    //public bool IsNeutralized { get { return Health <= 0; } }
+    public bool IsNeutralized => Health <= 0;
+    
+    public bool IsActive => !(IsNeutralized || HasScored);
+    
     public Invader(Path path)
     {
       _path = path;
     }
     
-    public void Move() _pathStep += 1; 
+    public void Move() => _pathStep += 1; 
     
     public void DecreaseHealth(int factor)
     {
