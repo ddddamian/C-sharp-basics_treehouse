0de7e431d911641623cbb8c36bb4096ec6b56982
Pete <peterwalsh86@hotmail.com>
2018-03-29 17:56:37 +0100

    Update Tower.cs

--- c-sharp-objects/Tower.cs
+++ c-sharp-objects/Tower.cs
@@ -2,6 +2,9 @@ namespace TreehouseDefense
 {
   class Tower
   {
+    private const int _range = 1;
+    private const int _power = 1;
+    
     private readonly MapLocation _location;
     
     public Tower(MapLocation location)
@@ -11,13 +14,11 @@ namespace TreehouseDefense
     
     public void FireOnInvaders(Invader[] invaders)
     {
-      
-      
-      foreach(Invader invader = invaders)
+      foreach(Invader invader in invaders)
       {
-        if(invader.IsActive && _location.InRangeOf(invader.location, 1))
+        if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
         {
-          invader.DecreaseHealth(1);
+          invader.DecreaseHealth(_power);
           break;
         }
         
